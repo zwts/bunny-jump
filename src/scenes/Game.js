17be89f29ed20ee6f663f9fb49799f1dc2924b82
Tan Shi <shi.tan@kaiostech.com>
2021-09-01 10:43:11 +0800

    Add  platfrom in Game scense

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,11 +8,15 @@ export default class Game extends Phaser.Scene
 
   //called to allow us to specify images, audio, or other assets to load before starting the Scene.
   preload() {
+    // The path `assets/bg_layer1.png` is not relative to the current file.
+    // second is the path to the image when served from the development server.
     this.load.image('background', 'assets/bg_layer1.png');
+    this.load.image('platform', 'assets/ground_grass.png');
   }
 
   //called once all the assets for the Scene have been loaded.
   create() {
     this.add.image(240, 320, 'background');
+    this.add.image(240, 320, 'platform').setScale(0.5);
   }
 }
