36c79c1c4e94df2b3add912d74bf68f50527b78c
Tan Shi <shi.tan@kaiostech.com>
2021-09-06 15:47:21 +0800

    Add group of physic platforms.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -18,7 +18,18 @@ export default class Game extends Phaser.Scene
   create() {
     this.add.image(240, 320, 'background');
     // this.add.image(240, 320, 'platform').setScale(0.5);
-    // this.physics.add.image(240, 320, 'platform').setScale(0.5);
-    this.physics.add.staticImage(240, 320, 'platform').setScale(0.5);
+    // this.physics.add.image(240, 320, 'platform').setScale(0.5);  a fall platform
+    // this.physics.add.staticImage(240, 320, 'platform').setScale(0.5); a static platform
+    const platforms = this.physics.add.staticGroup();
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      const platform = platforms.create(x, y, 'platform');
+      platform.scale = 0.5;
+
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
   }
 }
