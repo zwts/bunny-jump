7873c7bad46151cdc17e7473e7c469a244435ef1
Tan Shi <shi.tan@kaiostech.com>
2021-09-06 16:04:52 +0800

    Add player stand on platform.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,11 +12,15 @@ export default class Game extends Phaser.Scene
     // second is the path to the image when served from the development server.
     this.load.image('background', 'assets/bg_layer1.png');
     this.load.image('platform', 'assets/ground_grass.png');
+    this.load.image('bunny-stand', 'assets/bunny1_stand.png');
   }
 
   //called once all the assets for the Scene have been loaded.
   create() {
+    // create background
     this.add.image(240, 320, 'background');
+
+    // create platform
     // this.add.image(240, 320, 'platform').setScale(0.5);
     // this.physics.add.image(240, 320, 'platform').setScale(0.5);  a fall platform
     // this.physics.add.staticImage(240, 320, 'platform').setScale(0.5); a static platform
@@ -31,5 +35,9 @@ export default class Game extends Phaser.Scene
       const body = platform.body;
       body.updateFromGameObject();
     }
+
+    // create bunny
+    const player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+    this.physics.add.collider(platforms, player);
   }
 }
